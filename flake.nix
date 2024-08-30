5e5e8ed28797f72325dde359fd32085a804101c8
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2024-08-30 12:12:58 +0100

    flake package to build

--- flake.nix
+++ flake.nix
@@ -4,15 +4,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    zig = {
-      url = "github:mitchellh/zig-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { flake-utils, nixpkgs, zig, ... }: let
-      systems = builtins.attrNames zig.packages;
-    in flake-utils.lib.eachSystem systems (system:
+  outputs = { flake-utils, nixpkgs, ... }: 
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         duckdb-custom = pkgs.duckdb.overrideAttrs (oldAttrs: {
@@ -21,17 +16,51 @@
             cp -r $src/third_party/* $out/third_party
           '';
         });
-      in  {
-        devShells.default = pkgs.mkShell.override { stdenv = pkgs.libcxxStdenv; } {
-          name = "nixpkgs duckdb dev shell";
+      in {
+        packages.default = pkgs.stdenv.mkDerivation rec {
+          pname = "bazel-execlog-duckdb-extension";
+          version = "1.0.0";
 
+          src = ./.;
+
+          dontConfigure = true;
+
+          deps = pkgs.callPackage ./build.zig.zon.nix { };
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            gnumake
+            zig_0_13.hook
+          ];
+          
+          buildInputs = with pkgs; [
+            duckdb-custom
+            re2.dev
+          ] ++ pkgs.lib.optionals (pkgs.stdenv.isLinux) [ pkgs.libcxx ];
+
+          zigBuildFlags = [
+            "--system"
+            "${deps}"
+          ];
+
+          installPhase = ''
+            mkdir -p $out
+            ls -la
+            cp zig-out/lib/compact_execlog.duckdb_extension $out/compact_execlog.duckdb_extension
+          '';
+
+          DUCKDB_THIRD_PARTY_PATH = "${duckdb-custom}/third_party";
+          DUCKDB_DEV_PATH = "${duckdb-custom.dev}/";
+        };
+        
+        devShells.default = pkgs.mkShell.override { stdenv = pkgs.libcxxStdenv; } {
           DUCKDB_THIRD_PARTY_PATH = "${duckdb-custom}/third_party";
           DUCKDB_DEV_PATH = "${duckdb-custom.dev}/";
 
           nativeBuildInputs = with pkgs; [
             pkg-config
             gnumake
-            zig.packages.${system}."0.13.0"
+            zig_0_13
 	          zls
             duckdb-custom
             re2.dev
